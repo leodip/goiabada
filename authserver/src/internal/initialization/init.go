e14e39367ee10ec8394d09c943d57d5144f5f29c
Leonardo D'Ippolito <leodip@outlook.com>
2023-11-26 08:39:02 +0000

    Updating .gitignore

--- authserver/src/internal/initialization/init.go
+++ authserver/src/internal/initialization/init.go
@@ -1,15 +1,12 @@
 package initialization
 
 import (
-	"fmt"
-	"os"
 	"strings"
 	"time"
 
 	"log/slog"
 
 	"github.com/leodip/goiabada/internal/lib"
-	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 )
 
@@ -18,29 +15,11 @@ func InitViper() {
 	viper.SetDefault("StaticDir", "./static")
 	viper.SetDefault("TemplateDir", "./template")
 
-	viper.SetConfigName("config")
-	viper.SetConfigType("json")
-
-	// possible locations for config file
-	viper.AddConfigPath("./configs")
-	viper.AddConfigPath("../../configs") // for integration tests
-
 	viper.SetEnvPrefix("GOIABADA")
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-			// Config file was found but another error was produced
-			slog.Error(errors.Wrap(err, "unable to initialize configuration - make sure a config.json file exists and has content").Error())
-			os.Exit(1)
-		}
-	}
-
 	slog.Info("viper configuration initialized")
-	if len(viper.ConfigFileUsed()) > 0 {
-		slog.Info(fmt.Sprintf("viper config file used: %v", viper.ConfigFileUsed()))
-	}
 }
 
 func InitTimeZones() {
