b4ea127634b0797ab883b258b0a4d15d97d5176b
Leonardo D'Ippolito <contact@leodip.com>
2024-02-23 21:02:50 +0000

    Fix bug in login functionality

--- authserver/src/internal/datav2/sqlitedb/migrations/000001_initial_create.up.sql
+++ authserver/src/internal/datav2/sqlitedb/migrations/000001_initial_create.up.sql
@@ -2,16 +2,16 @@
 
 CREATE TABLE clients (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   client_identifier TEXT NOT NULL,
   client_secret_encrypted BLOB,
-  `description` TEXT DEFAULT NULL,
-  `enabled` BOOLEAN NOT NULL,
-  consent_required BOOLEAN NOT NULL,
-  is_public BOOLEAN NOT NULL,
-  authorization_code_enabled BOOLEAN NOT NULL,
-  client_credentials_enabled BOOLEAN NOT NULL,
+  `description` TEXT,
+  `enabled` numeric NOT NULL,
+  consent_required numeric NOT NULL,
+  is_public numeric NOT NULL,
+  authorization_code_enabled numeric NOT NULL,
+  client_credentials_enabled numeric NOT NULL,
   token_expiration_in_seconds int NOT NULL,
   refresh_token_offline_idle_timeout_in_seconds int NOT NULL,
   refresh_token_offline_max_lifetime_in_seconds int NOT NULL,
@@ -22,19 +22,19 @@ CREATE TABLE clients (
 
 CREATE TABLE resources (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   resource_identifier TEXT NOT NULL,
-  `description` TEXT DEFAULT NULL  
+  `description` TEXT  
 );
 
 
 CREATE TABLE permissions (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   permission_identifier TEXT NOT NULL,
-  `description` TEXT DEFAULT NULL,
+  `description` TEXT,
   resource_id INTEGER NOT NULL,      
   CONSTRAINT fk_permissions_resource FOREIGN KEY (resource_id) REFERENCES resources (id) ON DELETE CASCADE
 );
@@ -42,8 +42,8 @@ CREATE TABLE permissions (
 
 CREATE TABLE clients_permissions (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   client_id INTEGER NOT NULL,  
   permission_id INTEGER NOT NULL,      
   CONSTRAINT fk_clients_permissions_client FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
@@ -53,47 +53,47 @@ CREATE TABLE clients_permissions (
 
 CREATE TABLE users (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
-  `enabled` BOOLEAN NOT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
+  `enabled` numeric NOT NULL,
   `subject` TEXT NOT NULL,
   `username` TEXT NOT NULL,
-  given_name TEXT DEFAULT NULL,
-  middle_name TEXT DEFAULT NULL,
-  family_name TEXT DEFAULT NULL,
-  nickname TEXT DEFAULT NULL,
-  website TEXT DEFAULT NULL,
-  gender TEXT DEFAULT NULL,
-  email TEXT DEFAULT NULL,
-  email_verified BOOLEAN NOT NULL,
+  given_name TEXT,
+  middle_name TEXT,
+  family_name TEXT,
+  nickname TEXT,
+  website TEXT,
+  gender TEXT,
+  email TEXT,
+  email_verified numeric NOT NULL,
   email_verification_code_encrypted BLOB,
-  email_verification_code_issued_at DATETIME DEFAULT NULL,
-  zone_info_country_name TEXT DEFAULT NULL,
-  zone_info TEXT DEFAULT NULL,
-  locale TEXT DEFAULT NULL,
-  birth_date DATETIME DEFAULT NULL,
-  phone_number TEXT DEFAULT NULL,
-  phone_number_verified BOOLEAN NOT NULL,
+  email_verification_code_issued_at DATETIME,
+  zone_info_country_name TEXT,
+  zone_info TEXT,
+  locale TEXT,
+  birth_date DATETIME,
+  phone_number TEXT,
+  phone_number_verified numeric NOT NULL,
   phone_number_verification_code_encrypted BLOB,
-  phone_number_verification_code_issued_at DATETIME DEFAULT NULL,
-  address_line1 TEXT DEFAULT NULL,
-  address_line2 TEXT DEFAULT NULL,
-  address_locality TEXT DEFAULT NULL,
-  address_region TEXT DEFAULT NULL,
-  address_postal_code TEXT DEFAULT NULL,
-  address_country TEXT DEFAULT NULL,
+  phone_number_verification_code_issued_at DATETIME,
+  address_line1 TEXT,
+  address_line2 TEXT,
+  address_locality TEXT,
+  address_region TEXT,
+  address_postal_code TEXT,
+  address_country TEXT,
   password_hash TEXT NOT NULL,
-  otp_secret TEXT DEFAULT NULL,
-  otp_enabled BOOLEAN NOT NULL,
+  otp_secret TEXT,
+  otp_enabled numeric NOT NULL,
   forgot_password_code_encrypted BLOB,
-  forgot_password_code_issued_at DATETIME DEFAULT NULL
+  forgot_password_code_issued_at DATETIME
 );
 
 
 CREATE TABLE codes (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   code_hash TEXT NOT NULL,
   client_id INTEGER NOT NULL,
   code_challenge TEXT NOT NULL,
@@ -110,7 +110,7 @@ CREATE TABLE codes (
   session_identifier TEXT NOT NULL,
   acr_level TEXT NOT NULL,
   auth_methods TEXT NOT NULL,
-  used BOOLEAN NOT NULL,  
+  used numeric NOT NULL,  
   CONSTRAINT fk_codes_client FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
   CONSTRAINT fk_codes_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
@@ -118,23 +118,23 @@ CREATE TABLE codes (
 
 CREATE TABLE groups (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   group_identifier TEXT NOT NULL,
-  `description` TEXT DEFAULT NULL,
-  include_in_id_token BOOLEAN NOT NULL,
-  include_in_access_token BOOLEAN NOT NULL  
+  `description` TEXT,
+  include_in_id_token numeric NOT NULL,
+  include_in_access_token numeric NOT NULL  
 );
 
 
 CREATE TABLE group_attributes (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   `key` TEXT NOT NULL,
   `value` TEXT NOT NULL,
-  include_in_id_token BOOLEAN NOT NULL,
-  include_in_access_token BOOLEAN NOT NULL,
+  include_in_id_token numeric NOT NULL,
+  include_in_access_token numeric NOT NULL,
   group_id INTEGER NOT NULL,  
   CONSTRAINT fk_groups_attributes FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE
 );
@@ -142,8 +142,8 @@ CREATE TABLE group_attributes (
 
 CREATE TABLE groups_permissions (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   group_id INTEGER NOT NULL,
   permission_id INTEGER NOT NULL,    
   CONSTRAINT fk_groups_permissions_group FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE,
@@ -153,17 +153,17 @@ CREATE TABLE groups_permissions (
 
 CREATE TABLE http_sessions (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   `data` longtext,  
-  expires_on DATETIME DEFAULT NULL  
+  expires_on DATETIME  
 );
 
 
 CREATE TABLE key_pairs (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   `state` TEXT NOT NULL,
   key_identifier TEXT NOT NULL,
   `type` TEXT NOT NULL,
@@ -177,18 +177,18 @@ CREATE TABLE key_pairs (
 
 CREATE TABLE pre_registrations (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
-  email TEXT DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
+  email TEXT,
   password_hash TEXT NOT NULL,
   verification_code_encrypted BLOB,
-  verification_code_issued_at DATETIME DEFAULT NULL
+  verification_code_issued_at DATETIME
 );
 
 
 CREATE TABLE redirect_uris (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
   uri TEXT NOT NULL,
   client_id INTEGER NOT NULL,  
   CONSTRAINT fk_clients_redirect_uris FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
@@ -197,8 +197,8 @@ CREATE TABLE redirect_uris (
 
 CREATE TABLE refresh_tokens (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   code_id INTEGER NOT NULL,
   refresh_token_jti TEXT NOT NULL,
   previous_refresh_token_jti TEXT NOT NULL,
@@ -206,42 +206,42 @@ CREATE TABLE refresh_tokens (
   session_identifier TEXT NOT NULL,
   refresh_token_type TEXT NOT NULL,
   scope TEXT NOT NULL,
-  issued_at DATETIME DEFAULT NULL,
-  expires_at DATETIME DEFAULT NULL,
-  max_lifetime DATETIME DEFAULT NULL,
-  revoked BOOLEAN NOT NULL,    
+  issued_at DATETIME,
+  expires_at DATETIME,
+  max_lifetime DATETIME,
+  revoked numeric NOT NULL,    
   CONSTRAINT fk_refresh_tokens_code FOREIGN KEY (code_id) REFERENCES codes (id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE settings (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   app_name TEXT NOT NULL,
   issuer TEXT NOT NULL,
   ui_theme TEXT NOT NULL,
   password_policy int DEFAULT NULL,
-  self_registration_enabled BOOLEAN NOT NULL,
-  self_registration_requires_email_verification BOOLEAN NOT NULL,
+  self_registration_enabled numeric NOT NULL,
+  self_registration_requires_email_verification numeric NOT NULL,
   token_expiration_in_seconds int NOT NULL,
   refresh_token_offline_idle_timeout_in_seconds int NOT NULL,
   refresh_token_offline_max_lifetime_in_seconds int NOT NULL,
   user_session_idle_timeout_in_seconds int NOT NULL,
   user_session_max_lifetime_in_seconds int NOT NULL,
-  include_open_id_connect_claims_in_access_token BOOLEAN NOT NULL,
+  include_open_id_connect_claims_in_access_token numeric NOT NULL,
   session_authentication_key BLOB NOT NULL,
   session_encryption_key BLOB NOT NULL,
   aes_encryption_key BLOB NOT NULL,
-  smtp_host TEXT DEFAULT NULL,
+  smtp_host TEXT,
   smtp_port int DEFAULT NULL,
-  smtp_username TEXT DEFAULT NULL,
+  smtp_username TEXT,
   smtp_password_encrypted BLOB,
-  smtp_from_name TEXT DEFAULT NULL,
-  smtp_from_email TEXT DEFAULT NULL,
-  smtp_encryption TEXT DEFAULT NULL,
-  smtp_enabled BOOLEAN NOT NULL,
-  sms_provider TEXT DEFAULT NULL,
+  smtp_from_name TEXT,
+  smtp_from_email TEXT,
+  smtp_encryption TEXT,
+  smtp_enabled numeric NOT NULL,
+  sms_provider TEXT,
   sms_config_encrypted BLOB
 );
 
@@ -249,12 +249,12 @@ CREATE TABLE settings (
 
 CREATE TABLE user_attributes (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   `key` TEXT NOT NULL,
   `value` TEXT NOT NULL,
-  include_in_id_token BOOLEAN NOT NULL,
-  include_in_access_token BOOLEAN NOT NULL,
+  include_in_id_token numeric NOT NULL,
+  include_in_access_token numeric NOT NULL,
   user_id INTEGER NOT NULL,    
   CONSTRAINT fk_users_attributes FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
@@ -262,12 +262,12 @@ CREATE TABLE user_attributes (
 
 CREATE TABLE user_consents (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   user_id INTEGER NOT NULL,
   client_id INTEGER NOT NULL,
   scope TEXT NOT NULL,
-  granted_at DATETIME DEFAULT NULL,  
+  granted_at DATETIME,  
   CONSTRAINT fk_user_consents_client FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
   CONSTRAINT fk_user_consents_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
@@ -275,8 +275,8 @@ CREATE TABLE user_consents (
 
 CREATE TABLE user_sessions (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   session_identifier TEXT NOT NULL,
   `started` DATETIME NOT NULL,
   last_accessed DATETIME NOT NULL,
@@ -294,8 +294,8 @@ CREATE TABLE user_sessions (
 
 CREATE TABLE user_session_clients (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   user_session_id INTEGER NOT NULL,
   client_id INTEGER NOT NULL,
   `started` DATETIME NOT NULL,
@@ -307,8 +307,8 @@ CREATE TABLE user_session_clients (
 
 CREATE TABLE users_groups (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   group_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,  
   CONSTRAINT fk_users_groups_group FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE,
@@ -319,8 +319,8 @@ CREATE TABLE users_groups (
 
 CREATE TABLE users_permissions (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
-  updated_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
+  updated_at DATETIME,
   user_id INTEGER NOT NULL,
   permission_id INTEGER NOT NULL,  
   CONSTRAINT fk_users_permissions_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
@@ -330,7 +330,7 @@ CREATE TABLE users_permissions (
 
 CREATE TABLE web_origins (
   `id` integer PRIMARY KEY AUTOINCREMENT,
-  created_at DATETIME DEFAULT NULL,
+  created_at DATETIME,
   origin TEXT NOT NULL,
   client_id INTEGER NOT NULL,  
   CONSTRAINT fk_clients_web_origins FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
