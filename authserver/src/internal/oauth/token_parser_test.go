120e4a67e6b94ab3768b4e1a9e05853764afb9f8
Leonardo D'Ippolito <contact@leodip.com>
2024-08-31 21:56:20 +0100

    Refactor token validation to include offline access scope check

--- authserver/src/internal/oauth/token_parser_test.go
+++ authserver/src/internal/oauth/token_parser_test.go
@@ -179,6 +179,101 @@ func TestDecodeAndValidateTokenResponse_EmptyTokens(t *testing.T) {
 	assert.Nil(t, result.RefreshToken)
 }
 
+func TestDecodeAndValidateTokenString(t *testing.T) {
+	mockDB := mocks_data.NewDatabase(t)
+	tp := NewTokenParser(mockDB)
+
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	publicKey := &privateKey.PublicKey
+
+	tests := []struct {
+		name          string
+		tokenClaims   jwt.MapClaims
+		expectedError string
+	}{
+		{
+			name: "Valid token",
+			tokenClaims: jwt.MapClaims{
+				"sub": "1234567890",
+				"exp": time.Now().Add(time.Hour).Unix(),
+			},
+			expectedError: "",
+		},
+		{
+			name: "Expired token",
+			tokenClaims: jwt.MapClaims{
+				"sub": "1234567890",
+				"exp": time.Now().Add(-time.Hour).Unix(),
+			},
+			expectedError: "token has invalid claims: token is expired",
+		},
+		{
+			name: "Missing expiration",
+			tokenClaims: jwt.MapClaims{
+				"sub": "1234567890",
+			},
+			expectedError: "token has invalid claims: token is missing required claim: exp claim is required",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			token := jwt.NewWithClaims(jwt.SigningMethodRS256, tt.tokenClaims)
+			tokenString, _ := token.SignedString(privateKey)
+
+			result, err := tp.DecodeAndValidateTokenString(context.Background(), tokenString, publicKey)
+
+			if tt.expectedError != "" {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), tt.expectedError)
+				assert.Nil(t, result)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, result)
+				assert.Equal(t, tokenString, result.TokenBase64)
+				assert.Equal(t, tt.tokenClaims["sub"], result.Claims["sub"])
+			}
+		})
+	}
+}
+
+func TestDecodeAndValidateTokenString_InvalidSignature(t *testing.T) {
+	mockDB := mocks_data.NewDatabase(t)
+	tp := NewTokenParser(mockDB)
+
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	wrongPrivateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	publicKey := &privateKey.PublicKey
+
+	claims := jwt.MapClaims{
+		"sub": "1234567890",
+		"exp": time.Now().Add(time.Hour).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	tokenString, _ := token.SignedString(wrongPrivateKey)
+
+	result, err := tp.DecodeAndValidateTokenString(context.Background(), tokenString, publicKey)
+
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "token signature is invalid")
+	assert.Nil(t, result)
+}
+
+func TestDecodeAndValidateTokenString_EmptyToken(t *testing.T) {
+	mockDB := mocks_data.NewDatabase(t)
+	tp := NewTokenParser(mockDB)
+
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	publicKey := &privateKey.PublicKey
+
+	result, err := tp.DecodeAndValidateTokenString(context.Background(), "", publicKey)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, "", result.TokenBase64)
+	assert.Nil(t, result.Claims)
+}
+
 func createTestToken(privateKey *rsa.PrivateKey, claims map[string]interface{}, expirationTime time.Time) string {
 	token := jwt.New(jwt.SigningMethodRS256)
 	claims["exp"] = expirationTime.Unix()
