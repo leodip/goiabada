d74f10ba34b71cf8d39300c48c839910f3b848a4
Leonardo D'Ippolito <contact@leodip.com>
2026-02-05 20:36:42 +0000

    Fix formatting of comments in WellKnownConfig struct

--- src/core/oidc/well_known_config.go
+++ src/core/oidc/well_known_config.go
@@ -10,8 +10,8 @@ type WellKnownConfig struct {
 	RegistrationEndpoint              string   `json:"registration_endpoint,omitempty"` // RFC 7591 §4
 	GrantTypesSupported               []string `json:"grant_types_supported"`
 	ResponseTypesSupported            []string `json:"response_types_supported"`
-	ResponseModesSupported            []string `json:"response_modes_supported,omitempty"`      // OIDC Discovery 1.0 §3
-	PromptValuesSupported             []string `json:"prompt_values_supported,omitempty"`       // OIDC Discovery 1.0 §3
+	ResponseModesSupported            []string `json:"response_modes_supported,omitempty"` // OIDC Discovery 1.0 §3
+	PromptValuesSupported             []string `json:"prompt_values_supported,omitempty"`  // OIDC Discovery 1.0 §3
 	ACRValuesSupported                []string `json:"acr_values_supported"`
 	SubjectTypesSupported             []string `json:"subject_types_supported"`
 	IdTokenSigningAlgValuesSupported  []string `json:"id_token_signing_alg_values_supported"`
